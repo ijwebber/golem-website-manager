c2e61335e4e6da0e53f0c3a35f5b6aecffcf0df1
Isaac Webber <isaacjwebber@gmail.com>
2022-07-12 19:58:25 +0100

    Added error page

--- client/src/components/routes/Error.js
+++ client/src/components/routes/Error.js
@@ -1,7 +1,14 @@
+import './Error.css';
+import {Link} from 'react-router-dom';
+
 function Error() {
     return (
         <div className="Error">
-            404
+            <h1>404</h1>
+            <div>
+                <h2>PAGE NOT FOUND</h2>
+                <Link to="/"><button >HOME</button></Link>
+            </div>
         </div>
     );
 }
